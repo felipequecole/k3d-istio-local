6bfa0922d38ffcf22c494ad972a4e5ceded4ee41
Felipe Quécole <felipequecole@gmail.com>
2022-06-24 22:42:50 -0300

    Minor adjustments to boot-mesh script

--- boot-mesh.sh
+++ boot-mesh.sh
@@ -1,18 +1,15 @@
-# Create k3d cluster
-# k3d cluster create local-mesh --servers 1 --agents 2 --port 9080:80@loadbalancer --port 9443:443@loadbalancer --api-port 6443 --k3s-arg "--disable=traefik@server:0"
+#!/bin/bash
+
 echo "Creating k3d cluster"
 k3d cluster create --config ./k3d-cluster-config.yaml
 
 echo "Installing istio"
-# install istio
 istioctl install --set profile=demo 
 
 echo "Labeling default namespace to auto-enable sidecars"
-# Apply label to default namespace
 kubectl label namespace default istio-injection=enabled
 
 echo "Creating local Istio Gateway"
-# Apply gateway
 kubectl apply -f local-gw.yaml
 
 echo "You now have a local service mesh running!"
